14cb58b726ce6b440cca84c1dcc818277642b390
juangomez88 <60585685+juangomez88@users.noreply.github.com>
2022-03-13 22:00:54 -0500

    Rama Portafolio
    Realizo el cargue de la rama donde se encuentran los archivos del portafolio de los talleres realizados en cantera 1

---
+++ hojaDeVida/js/hoja.js
@@ -0,0 +1,101 @@
+
+const tonosAzules = () =>{
+
+    var element = document.getElementById("cabecera");
+    element.style.backgroundColor = 'rgb(166, 221, 243)';
+    element.style.fontFamily = "Arial narrow";
+    element.style.color = 'blue';
+
+    var element = document.getElementById("seccionImagen");
+    element.style.backgroundColor = 'rgb(166, 221, 243)';
+    element.style.fontFamily = "Arial narrow";
+
+    var element = document.getElementById("nombre");
+    element.style.color = 'blue';
+
+    var element = document.getElementById("carrera");
+    element.style.color = 'blue';
+    
+
+    var element = document.getElementById("parrafo");
+    element.style.fontFamily = "ArialNarrow";
+    element.style.backgroundColor ='rgb(166, 221, 243)';
+    element.style.color = 'blue';
+
+
+    var element = document.getElementById("final");
+    element.style.backgroundColor ='rgb(166, 221, 243)';
+    element.style.fontFamily = "ArialNarrow";
+    element.style.color = 'blue';
+
+    var element = document.getElementById("titleTable");
+    element.style.backgroundColor = 'blue';
+    element.style.fontFamily = "ArialNarrow";
+    element.style.color = 'blue';
+
+
+}
+
+const tonosRojos= () =>{
+
+    var element = document.getElementById("cabecera");
+    element.style.backgroundColor = 'rgba(240, 48, 48, 0.637)';
+    element.style.fontFamily = "Lucida Sans";
+    element.style.color = 'black';
+
+    var element = document.getElementById("seccionImagen");
+    element.style.backgroundColor = 'rgba(240, 48, 48, 0.637)';
+    element.style.fontFamily = "Lucida Sans";
+
+    var element = document.getElementById("nombre");
+    element.style.color = 'black';
+
+    var element = document.getElementById("carrera");
+    element.style.color = 'black';
+    
+
+    var element = document.getElementById("parrafo");
+    element.style.fontFamily = "ArialNarrow";
+    element.style.backgroundColor ='rgba(240, 48, 48, 0.637)';
+    element.style.color = 'black';
+
+
+    var element = document.getElementById("final");
+    element.style.backgroundColor ='rgba(240, 48, 48, 0.637)';
+    element.style.fontFamily = "ArialNarrow";
+    element.style.color = 'black';
+
+}
+
+const tonosVerdes= () =>{
+
+    var element = document.getElementById("cabecera");
+    element.style.backgroundColor = 'rgba(41, 102, 41, 0.637)';
+    element.style.fontFamily = "Lucida Sans";
+    element.style.color = 'black';
+
+    var element = document.getElementById("seccionImagen");
+    element.style.backgroundColor = 'rgba(41, 102, 41, 0.637)';
+    element.style.fontFamily = "Lucida Sans";
+
+    var element = document.getElementById("nombre");
+    element.style.color = 'black';
+
+    var element = document.getElementById("carrera");
+    element.style.color = 'black';
+    
+
+    var element = document.getElementById("parrafo");
+    element.style.fontFamily = "ArialNarrow";
+    element.style.backgroundColor ='rgba(5, 80,21)';
+    element.style.color = 'burlywood';
+
+
+    var element = document.getElementById("final");
+    element.style.backgroundColor = 'rgba(41, 102, 41, 0.637)';
+    element.style.fontFamily = "ArialNarrow";
+    element.style.color = 'black';
+
+}
+
+
